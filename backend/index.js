de057b65dc18e528fad83425f59190d8fedf72a1
matheuscarvalhoscm <matheuscarvalhoscm@gmail.com>
2022-02-15 18:07:57 -0300

    [BACKEND] CORS setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,15 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
 const taskRouter = require('./routes/taskRouter');
 
 const app = express();
 const PORT = 3001;
 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}));
 app.use(bodyParser.json());
 app.use('/', taskRouter);
 
